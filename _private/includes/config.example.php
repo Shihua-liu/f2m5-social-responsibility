cf21b33f44d23fbf5372b9d7f038ba75bebc7960
shihua liu <liushihua2004@gmail.com>
2021-09-16 10:55:41 +0200

    composer gedownload en toegevoegd

--- _private/includes/config.example.php
+++ _private/includes/config.example.php
@@ -8,7 +8,7 @@ $config = [
 		'HOSTNAME' => '127.0.0.1',
 		'DATABASE' => '',
 		'USER'     => 'root',
-		'PASSWORD' => ''
+		'PASSWORD' => 'root'
 	],
 	'BASE_URL' => '',  // Zet hier het pad naar de public map in, vanaf http://localhost, anders werken je routes niet!
 	'PUBLIC'     => dirname( __DIR__, 2 ),  // Vanaf dit bestand 2 mappen omhoog
