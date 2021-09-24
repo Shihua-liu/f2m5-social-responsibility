74cb6dd6cc5720157806935a1427b6b661f154f9
shihua liu <liushihua2004@gmail.com>
2021-09-24 13:15:23 +0200

    submit button een color gegeven en registreer button op homepage werkend

--- _private/views/_navigation.php
+++ _private/views/_navigation.php
@@ -1,6 +1,6 @@
 <ul>
     <li>
         <a href="<?php echo url( 'home' ) ?>"<?php if ( current_route_is( 'home' ) ): ?> class="active"<?php endif ?>>Home</a>
-        <a href="<?php echo url( 'register.form' ) ?>"<?php if ( current_route_is( 'register.form' ) ): ?> class="active"<?php endif ?>>registreer</a>
+        <a href="<?php echo url( 'register_form' ) ?>"<?php if ( current_route_is( 'register_form' ) ): ?> class="active"<?php endif ?>>registreer</a>
     </li>
 </ul>
