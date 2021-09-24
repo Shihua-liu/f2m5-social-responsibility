fe504099d3beaefa0b92ab41e0c2213be3eb7f8e
shihua liu <liushihua2004@gmail.com>
2021-09-24 13:01:46 +0200

    in routes een path gemaakt, register form aangemaakt, registratiecontroller aangemaakt en in navigation een button gemaakt, werkt momenteel niet

---
+++ _private/views/register_form.php
@@ -0,0 +1,26 @@
+<?php $this->layout('website')?>
+
+<h3>inschrijven</h3>
+
+<form action="" method="POST">
+    <div class="form-group">
+        <label for="voornaam">voornaam</label>
+        <input type="text" name="firstnaam" value="" class="form-control" id="voornaam">
+    </div>
+
+    <div class="form-group">
+        <label for="achternaam">achternaam</label>
+        <input type="text" name="lastname" value="" class="form-control" id="achternaam">
+    </div>
+
+    <div class="form-group">
+        <label for="email">email</label>
+        <input type="email" name="email" value="" class="form-control" id="email">
+    </div>
+
+    <div class="form-group">
+        <label for="wachtwoord">wachtwoord</label>
+        <input type="password" name="password" value="" class="form-control" id="wachtwoord">
+    </div>
+    <button type="submit" class="btn submit-btn">registreer</button>
+</form>
