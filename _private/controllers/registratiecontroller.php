5e14eebe8a970994207ba412fb1a759b8b92be99
shihua liu <liushihua2004@gmail.com>
2021-09-29 21:34:05 +0200

    database gelinked en problemen

--- _private/controllers/registratiecontroller.php
+++ _private/controllers/registratiecontroller.php
@@ -2,6 +2,8 @@
 
 namespace Website\Controllers;
 
+use Error;
+
 /**
  * Class WebsiteController
  *
@@ -19,7 +21,43 @@ class registratiecontroller {
 
 	}
 	public function registerprocess() {
-		echo ("FORM komt er aan!!");
+		
+		$error = [];
+		$email = filter_var ($_POST['email'], FILTER_VALIDATE_EMAIL);
+		$wachtwoord = trim ($_POST['wachtwoord']);
+
+		if ($email === false) {
+			$error['email'] = 'geen geldig email ingevult';
+		}
+
+		if (empty($wachtwoord) || strlen($wachtwoord)<6){
+			$error['wachtwoord'] = 'geen geldig wachtwoord ingevult, moet teminste 6 tekens bevatten';
+		}
+
+		if (count ($error) === 0 ){
+			$connection = dbConnect();
+			$sql = "SELECT * FROM 'gebruikers' WHERE 'email' = :email";
+			$statement = $connection->prepare($sql);
+			$statement->execute(['email'=>$email]);
+		}
+
+		if ($statement->rowCount() === 0){
+			$sql = "INSERT INTO 'gebruikers' ('email', 'wachtwoord') VALUES (:email, :wachtwoord)";
+			$statement = $connection->prepare($sql);
+			$safe_password = password_hash($wachtwoord, PASSWORD_DEFAULT);
+			$params =[
+				'email' => $email,
+				'wachtwoord' => $safe_password
+			];
+			$statement->execute($params);
+			echo "klaar";
+			exit;
+		}
+		else {
+			$error['email'] = 'email bestaat al';
+		}
 	}
+
+
 }
 
