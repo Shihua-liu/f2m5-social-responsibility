5e9052fe4aafa33b3d383ae7d1b1dd10b1c5af97
shihua liu <liushihua2004@gmail.com>
2021-09-29 22:55:29 +0200

    data base gelinked en voornaam en achternaam van form weggehaalt

--- _private/views/register_form.php
+++ _private/views/register_form.php
@@ -3,15 +3,6 @@
 <h3>inschrijven</h3>
 
 <form action="<?php echo url("register.handle")?>" method="POST">
-    <div class="form-group">
-        <label for="voornaam">voornaam</label>
-        <input type="text" name="firstnaam" value="" class="form-control" id="voornaam">
-    </div>
-
-    <div class="form-group">
-        <label for="achternaam">achternaam</label>
-        <input type="text" name="lastname" value="" class="form-control" id="achternaam">
-    </div>
 
     <div class="form-group">
         <label for="email">email</label>
